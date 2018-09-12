a26d2fe9287c59f57be8042e18b8e4a27247983c
Kurt Madel <kmadel@cloudbees.com>
2018-09-12 15:08:30 -0400

    Update hello.js
    Added console output for request received

--- hello.js
+++ hello.js
@@ -7,6 +7,7 @@ app.set('view engine', 'pug')
 var info = {buildNumber: process.env.BUILD_NUMBER}
 
 app.get('/', function (req, res) {
+  console.log('Hello World request.');
   res.render('index', { title: 'Hello', message: 'Hello World from {your_name}!', info })
 })
 
