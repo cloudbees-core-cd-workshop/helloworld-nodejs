7d51e60ed60aa75784f4ed05132cd9bb4a8a4f81
Kurt Madel <kmadel@mac.com>
2018-09-14 15:42:37 -0400

    use innerText instead of value

--- tests/index.test.js
+++ tests/index.test.js
@@ -7,5 +7,5 @@ test('Check message header', async t => {
    const messageHeader = Selector('#message');
 
    await t
-       .expect(messageHeader.value).contains('Hello World', 'message header contains text "Hello World"');
+       .expect(messageHeader.innerText).contains('Hello World', 'message header contains text "Hello World"');
 });
